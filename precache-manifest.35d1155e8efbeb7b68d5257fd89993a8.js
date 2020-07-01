7bb9b25146f659f58d4c142b3e101328650f05ae
Simona <see.moona@yahoo.com>
2020-07-01 15:14:33 +0200

    Updates

---
+++ precache-manifest.35d1155e8efbeb7b68d5257fd89993a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4e55a5fa65b40fc89e66d2652d06024",
+    "url": "/online-shop-w--redux/index.html"
+  },
+  {
+    "revision": "443d399ad8a06b64b2ba",
+    "url": "/online-shop-w--redux/static/css/main.94d32115.chunk.css"
+  },
+  {
+    "revision": "a20a9629391ff0f53735",
+    "url": "/online-shop-w--redux/static/js/2.8db8cce5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/online-shop-w--redux/static/js/2.8db8cce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "443d399ad8a06b64b2ba",
+    "url": "/online-shop-w--redux/static/js/main.9d976b93.chunk.js"
+  },
+  {
+    "revision": "903bedbfeb33ff94228c",
+    "url": "/online-shop-w--redux/static/js/runtime-main.8064106a.js"
+  }
+]);
