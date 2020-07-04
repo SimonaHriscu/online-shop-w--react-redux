8351e3079a06d970a42b53f00fe4930a3e9fce95
Simona <see.moona@yahoo.com>
2020-07-04 19:37:15 +0200

    color applied to inventory

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -4,37 +4,51 @@ import { addProduct, removeOne } from "../actions";
 
 class ProductItem extends React.Component {
   render() {
-   // console.log(this.props);
+   // console.log(this.props.info.inventory);
     const { info, addProduct, removeOne } = this.props;
+    
+    //Changing classes in the scss // adding extra text
+    let classes = [];
+    let extraText = "";
+    if (info.inventory <= 3) {
+      classes.push("out-of-stock");
+      extraText = "Last";
+    }
+
     return (
       <li>
         <span>
           <div className="product-title">
-           <p> {info.productName}</p> <i>{info.icon}</i>
-           </div>
+            <p> {info.productName}</p> <i>{info.icon}</i>
+          </div>
           <div className="product-price">{info.price}€</div>
           <div className="product-inventory">
-            {info.inventory > 0
-              ? `x ${info.inventory} items left`
-              : <div className="out-of-stock">Out of stock</div>}
+            {info.inventory > 0 ? (
+              <div className={classes}>
+                {extraText} {info.inventory} items left{" "}
+              </div>
+            ) : (
+              <div className="out-of-stock">Out of stock</div>
+            )}
           </div>
         </span>
 
+        <button
+          className="product-add"
+          onClick={() => addProduct(info)}
+          disabled={info.inventory === 0}
+        >
+          {info.inventory > 0 ? "Add to cart" : "Sold out"}
+        </button>
+        <button
+          className="product-add"
+          onClick={() => removeOne(info)}
+          disabled={info.inventory < 0}
+        >
+         
+          {info.inventory >= 0 ? "Delete an Item" : "Cart is empty"}
+        </button>
         
-          <button className="product-add"
-            onClick={() => addProduct(info)}
-            disabled={info.inventory === 0}
-          >
-            {info.inventory > 0 ? "Add to cart" : "Sold out"}
-          </button>
-          <button className="product-add"
-            onClick={() => removeOne(info)}
-            disabled={info.inventory === 0}
-          >
-            {info.inventory > 0 ? "Delete an Item" : "Cart is empty"}
-          </button>
-       
-
       </li>
     );
   }
